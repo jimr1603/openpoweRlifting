c36b0352aee107d320ac61386a7a39fc6f74af7e
James Riley <jimr1603@gmail.com>
2022-01-04 17:55:03 +0000

    Passes check if cran=FALSE. Tidied up the documentation on the functions a little.

--- R/wrappers.R
+++ R/wrappers.R
@@ -1,28 +1,25 @@
 
 
-#' Title
+#' Utility function for pinning the latest full OPL extract.
 #'
-#' @param board
-#' @param name
+#' @param board the Board object from {pins} to attach the data to. Defaults to pins::board_local().
+#' @param name optional STRING for the name for the pin. Defaults to "opl-ipf" or "opl-all"
 #'
-#' @return
+#' @return Same as pin_write
 #' @export
-#'
-#' @examples
 #'
 pin_opl_full = function(board = pins::board_local(), name = NULL){
   pin_opl(board, ipf = FALSE, name)
 }
 
-#' Title
+#' Utility function for pinning the latest IPF-only OPL extract.
 #'
-#' @param board
-#' @param name
+#' @param board the Board object from {pins} to attach the data to. Defaults to pins::board_local().
+#' @param name optional STRING for the name for the pin. Defaults to "opl-ipf" or "opl-all"
 #'
-#' @return
+#' @return Same as pin_write
 #' @export
 #'
-#' @examples
 pin_opl_ipf = function(board = pins::board_local(), name = NULL){
   pin_opl(board, ipf = TRUE, name)
 }
